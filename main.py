a9a49b77586b831e2acc84a0fb9ab04cdfc8c078
Danny <danny@audetmagna.co.uk>
2025-04-28 01:56:30 +0200

    v3, Sl not needed

--- main.py
+++ main.py
@@ -8,63 +8,61 @@ account = 40610362
 password = "MyDeriv123!"
 server = "Deriv-Demo"
 
-if not mt5.login(account,password, server):
+if not mt5.login(account, password, server):
     print("NOT WORKING")
     mt5.shutdown()
     exit()
 
-crash = "Crash 1000 Index"
-
-def open_trade(symbol, lot, order_type, sl_value, tp_value, deviation=20):
+def open_trade(symbol, lot, order_type, sl_value=None, tp_value=None, deviation=20):
     price = mt5.symbol_info_tick(symbol)
 
     if order_type == mt5.ORDER_TYPE_BUY:
         order_price = price.ask
-        sl = sl_value
-        tp = tp_value
-        print("buy")
     elif order_type == mt5.ORDER_TYPE_SELL:
         order_price = price.bid
-        sl = sl_value
-        tp = tp_value
-        print("sell")
 
-    print("order")
+    else:
+        print("Invalid order type")
+        return
+
+    print("sending order...")
+
     request = {
         "action": mt5.TRADE_ACTION_DEAL,
         "symbol": symbol,
         "volume": lot,
         "type": order_type,
         "price": order_price,
-        "sl": sl,
-        "tp": tp,
         "deviation": deviation,
         "magic": 234000,
         "comment": "python script open",
         "type_time": mt5.ORDER_TIME_GTC,
     }
+
+    if sl_value is not None:
+        request["sl"] = sl_value
+    if tp_value is not None:
+        request["tp"] = tp_value
+
     result = mt5.order_send(request)
     print(result)
     if result is None:
         print("order_send() failed, result is None")
 
-
-def get_chart(symbol,timeframe, bars = 1000):
+def get_chart(symbol, timeframe, bars=1000):
     time_from = datetime.now()
 
     rates = mt5.copy_rates_from(symbol, timeframe, time_from, bars)
    
     df = pd.DataFrame(rates)
-    df['time'] = pd.to_datetime(df['time'], unit='s')  # Convert timestamp
+    df['time'] = pd.to_datetime(df['time'], unit='s')
     return df
 
-a = get_chart(crash,mt5.TIMEFRAME_M5)
-print(a)
 
-a = get_chart(crash,mt5.TIMEFRAME_H1)
-print(a)
 
-a = get_chart(crash,mt5.TIMEFRAME_H4)
-print(a)
-"""open_trade(crash,0.2,mt5.ORDER_TYPE_BUY,4500.00,5100.00)
-open_trade(crash,1.0,mt5.ORDER_TYPE_SELL, 5100.00,4500.00)"""
+crash1000 = "Crash 1000 Index"
+for i in range(0, 10):
+
+    open_trade(crash1000, 50.00, mt5.ORDER_TYPE_BUY)
+
+    open_trade(crash1000, 50.00, mt5.ORDER_TYPE_BUY, 4500.00, 5100.00)
